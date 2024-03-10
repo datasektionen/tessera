631fb01d7a2f9f3ecac732aff3ac4dff39c21c5c
Lucas Dow <lucdow7@gmail.com>
2024-03-10 20:26:26 +0100

    Add animation and intersection observer to ContactDetails component

--- src/pages/main/get_in_touch.tsx
+++ src/pages/main/get_in_touch.tsx
@@ -8,11 +8,37 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn"; // import LinkedIn icon
 import GithubIcon from "@mui/icons-material/GitHub"; // import Github icon'
 import { useMediaQuery } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 const ContactDetails = () => {
   const theme = useTheme();
   const isScreenSmall = useMediaQuery(theme.breakpoints.down("sm"));
   const { t } = useTranslation();
+
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
+  const fadeInFromLeft = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 1.0, type: "spring" },
+    },
+  };
+
+  const fadeInFromRight = {
+    hidden: { opacity: 0, x: 100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { duration: 1.0, type: "spring" },
+    },
+  };
+
   return (
     <Grid
       container
@@ -31,19 +57,27 @@ const ContactDetails = () => {
           textAlign: isScreenSmall ? "center" : "right",
         }}
       >
-        <Box>
-          <img
-            src={ProfilePicture}
-            alt="Profile Picture"
-            style={{
-              width: "250px",
-              height: "250px",
-              borderRadius: "50%",
-              border: `10px solid ${PALLETTE.cerise_dark}`,
-            }}
-          />
-        </Box>
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          animate={inView ? "visible" : "hidden"}
+          variants={fadeInFromLeft}
+        >
+          <Box>
+            <img
+              src={ProfilePicture}
+              alt="Profile Picture"
+              style={{
+                width: "250px",
+                height: "250px",
+                borderRadius: "50%",
+                border: `10px solid ${PALLETTE.cerise_dark}`,
+              }}
+            />
+          </Box>
+        </motion.div>
       </Grid>
+
       <Grid
         sm={12}
         md={6}
@@ -51,63 +85,72 @@ const ContactDetails = () => {
           textAlign: isScreenSmall ? "center" : "left",
         }}
       >
-        <StyledText
-          level="body1"
-          color={PALLETTE.cerise_dark}
-          fontSize={32}
-          sx={{
-            mb: 1,
-          }}
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          animate={inView ? "visible" : "hidden"}
+          variants={fadeInFromRight}
         >
-          Lucas Dow
-        </StyledText>
-        <StyledText
-          level="body2"
-          color={PALLETTE.charcoal}
-          style={{
-            fontStyle: "italic", // Correct property for italic text
-          }}
-          sx={{
-            mb: 2,
-          }}
-        >
-          "{t("main_page.get_in_touch.quote1")}
-          <br />
-          <br />
-          {t("main_page.get_in_touch.quote2")}"
-        </StyledText>
-
-        <StyledText level="body2" color={PALLETTE.charcoal}>
-          <Link href="mailto:lucdow7@gmail.com">lucdow7@gmail.com</Link>
-        </StyledText>
-        <StyledText
-          level="body2"
-          color={PALLETTE.charcoal}
-          startDecorator={<PhoneIcon />}
-          style={{
-            justifyContent: isScreenSmall ? "center" : "flex-start",
-          }}
-        >
-          073 551 00 01
-        </StyledText>
-        <Stack
-          spacing={1}
-          direction="row"
-          mt={3}
-          justifyContent={isScreenSmall ? "center" : "flex-start"}
-        >
-          <Link
-            href="https://www.linkedin.com/in/lucas-dow-1315a61b5/"
-            target="_blank"
+          <StyledText
+            level="body1"
+            color={PALLETTE.cerise_dark}
+            fontSize={32}
+            sx={{
+              mb: 1,
+            }}
           >
-            <LinkedInIcon
-              style={{ color: PALLETTE.cerise_dark, fontSize: 32 }}
-            />{" "}
-          </Link>
-          <Link href="https://github.com/DowLucas" target="_blank">
-            <GithubIcon style={{ color: PALLETTE.cerise_dark, fontSize: 32 }} />
-          </Link>
-        </Stack>
+            Lucas Dow
+          </StyledText>
+          <StyledText
+            level="body2"
+            color={PALLETTE.charcoal}
+            style={{
+              fontStyle: "italic", // Correct property for italic text
+            }}
+            sx={{
+              mb: 2,
+            }}
+          >
+            "{t("main_page.get_in_touch.quote1")}
+            <br />
+            <br />
+            {t("main_page.get_in_touch.quote2")}"
+          </StyledText>
+
+          <StyledText level="body2" color={PALLETTE.charcoal}>
+            <Link href="mailto:lucdow7@gmail.com">lucdow7@gmail.com</Link>
+          </StyledText>
+          <StyledText
+            level="body2"
+            color={PALLETTE.charcoal}
+            startDecorator={<PhoneIcon />}
+            style={{
+              justifyContent: isScreenSmall ? "center" : "flex-start",
+            }}
+          >
+            073 551 00 01
+          </StyledText>
+          <Stack
+            spacing={1}
+            direction="row"
+            mt={3}
+            justifyContent={isScreenSmall ? "center" : "flex-start"}
+          >
+            <Link
+              href="https://www.linkedin.com/in/lucas-dow-1315a61b5/"
+              target="_blank"
+            >
+              <LinkedInIcon
+                style={{ color: PALLETTE.cerise_dark, fontSize: 32 }}
+              />{" "}
+            </Link>
+            <Link href="https://github.com/DowLucas" target="_blank">
+              <GithubIcon
+                style={{ color: PALLETTE.cerise_dark, fontSize: 32 }}
+              />
+            </Link>
+          </Stack>
+        </motion.div>
       </Grid>
     </Grid>
   );
