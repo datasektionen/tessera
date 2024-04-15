a3e23b8fa3c3bdeaac2740a24f6b077f1a3a694c
Lucas Dow <lucdow7@gmail.com>
2024-04-15 11:12:22 +0200

    Add new routes for page imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,51 @@
 // Import statements should be at the top
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ComponentType, useEffect } from "react";
+import React, {
+  ComponentType,
+  startTransition,
+  Suspense,
+  useEffect,
+} from "react";
 import { useDispatch } from "react-redux";
 import { ROUTES } from "./def";
-import WelcomePage from "../pages/welcome";
-import MainPage from "../pages/main";
 import ProtectedRoute from "../components/login/PrivateRoute";
-import EventDetail from "../pages/event/detail";
 import { currentUserRequest } from "../redux/features/userSlice";
-import ProfilePage from "../pages/profile";
-import CreateOrganizationPage from "../pages/organization/create";
-import ProfileOrganizationsPage from "../pages/profile/profile_organizations";
 import Logout from "../components/user/logout";
-import EventsPage from "../pages/event";
-import CreateEventPage from "../pages/event/create";
-import EditEventPage from "../pages/event/edit";
-import ProfileTicketRequestsPage from "../pages/profile/profile_ticket_requests";
-import ProfileTicketsPage from "../pages/profile/profile_tickets";
-import EditEventAddTicketReleasePage from "../pages/event/edit_ticket_release";
-import EditTicketTypes from "../pages/event/edit_ticket_types";
-import ManageEventPage from "../pages/event/manage";
-import External from "../pages/external";
-import HandleLoginCallback from "../pages/login/callback";
-import ExternalVerifyEmail from "../pages/external/verify-email";
-import ContactPage from "../pages/contact";
-import FourOFour404 from "../pages/errors/404";
-import TicketScannerPage from "../pages/event/ticket_scanner";
-import ForgotPassword from "../pages/external/forgot_password";
-import PasswordReset from "../pages/external/password_reset";
-import SendOut from "../components/events/send_out";
-import VerifyPreferredEmail from "../pages/user/preferred_email/verify";
-import EventEconomyPage from "../pages/event/economy";
-import EditTicketReleaseAddonsPage from "../pages/event/edit_addons";
-import ManageEventTicketReleasesPage from "../pages/event/manage/ticket_releases";
-import ManageEventTicketsPage from "../pages/event/manage/tickets";
-import PrivacyPolicy from "../components/legal/privacy";
-import ManageEventFormResponsesPage from "../pages/event/manage/ticket_form_reponse_list_page";
+import TesseraWrapper from "../components/wrappers/page_wrapper";
+import LoadingOverlay from "../components/Loading";
+
+import {
+  MainPage,
+  ProfilePage,
+  EditEventAddTicketReleasePage,
+  EditTicketTypes,
+  ProfileTicketRequestsPage,
+  ProfileTicketsPage,
+  ProfileOrganizationsPage,
+  CreateOrganizationPage,
+  ManageEventPage,
+  TicketScannerPage,
+  EventEconomyPage,
+  EditTicketReleaseAddonsPage,
+  ManageEventTicketReleasesPage,
+  ManageEventTicketsPage,
+  ManageEventFormResponsesPage,
+  FourOFour404,
+  EventsPage,
+  CreateEventPage,
+  EditEventPage,
+  HandleLoginCallback,
+  External,
+  ForgotPassword,
+  PasswordReset,
+  ExternalVerifyEmail,
+  VerifyPreferredEmail,
+  ContactPage,
+  SendOut,
+  PrivacyPolicy,
+  EventDetail,
+  WelcomePage,
+} from "./page_import";
 
 function withCurrentUserRequest<P>(
   Component: ComponentType<P>
@@ -46,7 +56,9 @@ function withCurrentUserRequest<P>(
     const dispatch = useDispatch();
 
     useEffect(() => {
-      dispatch(currentUserRequest());
+      startTransition(() => {
+        dispatch(currentUserRequest());
+      });
     }, [dispatch]);
 
     return <Component {...props} />;
@@ -101,106 +113,117 @@ const ManageEventFormResponsesWithCurrentUser = withCurrentUserRequest(
 function AppRoutes() {
   return (
     <BrowserRouter basename="/">
-      <Routes>
-        <Route path={ROUTES.LOGIN} element={<WelcomePage />} />
-        <Route
-          path={ROUTES.HANDLE_LOGIN_CALLBACK}
-          element={<HandleLoginCallback />}
-        />
-        <Route path={ROUTES.EXTERNAL} element={<External />} />
-        <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
-        <Route path={ROUTES.PASSWORD_RESET} element={<PasswordReset />} />
-        <Route
-          path={ROUTES.EXTERNAL_VERIFY_EMAIL}
-          element={<ExternalVerifyEmail />}
-        />
-        <Route
-          path={ROUTES.VERIFY_PREFERRED_EMAIL}
-          element={<VerifyPreferredEmail />}
-        />
-        <Route path={ROUTES.MAIN} element={<MainPage />} />
+      <Suspense
+        fallback={
+          <TesseraWrapper>
+            <LoadingOverlay />
+          </TesseraWrapper>
+        }
+      >
+        <Routes>
+          <Route path={ROUTES.LOGIN} element={<WelcomePage />} />
+          <Route
+            path={ROUTES.HANDLE_LOGIN_CALLBACK}
+            element={<HandleLoginCallback />}
+          />
+          <Route path={ROUTES.EXTERNAL} element={<External />} />
+          <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
+          <Route path={ROUTES.PASSWORD_RESET} element={<PasswordReset />} />
+          <Route
+            path={ROUTES.EXTERNAL_VERIFY_EMAIL}
+            element={<ExternalVerifyEmail />}
+          />
+          <Route
+            path={ROUTES.VERIFY_PREFERRED_EMAIL}
+            element={<VerifyPreferredEmail />}
+          />
+          <Route path={ROUTES.MAIN} element={<MainPage />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path={ROUTES.LOGOUT} element={<Logout />} />
-          <Route path={ROUTES.CONTACT_PAGE} element={<ContactPage />} />
-          <Route
-            path={ROUTES.EVENT_DETAIL}
-            element={<EventDetailWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.PROFILE}
-            element={<ProfilePageWithCurrentUser />}
-          />
-          <Route path={ROUTES.EVENTS} element={<EventsPageWithCurrentUser />} />
-          <Route
-            path={ROUTES.CREATE_EVENT}
-            element={<CreateEventPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.EDIT_EVENT}
-            element={<EditEventPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.EDIT_EVENT_ADD_TICKET_RELEASE}
-            element={<EditEventAddTicketReleasePageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.EDIT_EVENT_TICKET_RELEASE_TICKET_TYPES}
-            element={<EditTicketTypesWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.EDIT_EVENT_TICKET_RELEASE_ADDONS}
-            element={<EditTicketReleaseAddonsWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.PROFILE_TICKET_REQUESTS}
-            element={<ProfileTicketRequestsPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.PROFILE_TICKETS}
-            element={<ProfileTicketsPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.PROFILE_ORGANIZATIONS}
-            element={<ProfileOrganizationsPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.CREATE_ORGANIZATION}
-            element={<CreateOrganizationPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.MANAGE_EVENT}
-            element={<ManageEventPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.MANAGE_EVENT_TICKET_RELEASES}
-            element={<ManageEventTicketReleasesWithCurrentUser />}
-          />
+          <Route element={<ProtectedRoute />}>
+            <Route path={ROUTES.LOGOUT} element={<Logout />} />
+            <Route path={ROUTES.CONTACT_PAGE} element={<ContactPage />} />
+            <Route
+              path={ROUTES.EVENT_DETAIL}
+              element={<EventDetailWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.PROFILE}
+              element={<ProfilePageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.EVENTS}
+              element={<EventsPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.CREATE_EVENT}
+              element={<CreateEventPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.EDIT_EVENT}
+              element={<EditEventPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.EDIT_EVENT_ADD_TICKET_RELEASE}
+              element={<EditEventAddTicketReleasePageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.EDIT_EVENT_TICKET_RELEASE_TICKET_TYPES}
+              element={<EditTicketTypesWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.EDIT_EVENT_TICKET_RELEASE_ADDONS}
+              element={<EditTicketReleaseAddonsWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.PROFILE_TICKET_REQUESTS}
+              element={<ProfileTicketRequestsPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.PROFILE_TICKETS}
+              element={<ProfileTicketsPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.PROFILE_ORGANIZATIONS}
+              element={<ProfileOrganizationsPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.CREATE_ORGANIZATION}
+              element={<CreateOrganizationPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.MANAGE_EVENT}
+              element={<ManageEventPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.MANAGE_EVENT_TICKET_RELEASES}
+              element={<ManageEventTicketReleasesWithCurrentUser />}
+            />
 
-          <Route
-            path={ROUTES.MANAGE_EVENT_TICKETS}
-            element={<ManageEventTicketsWithCurrentUser />}
-          />
+            <Route
+              path={ROUTES.MANAGE_EVENT_TICKETS}
+              element={<ManageEventTicketsWithCurrentUser />}
+            />
 
-          <Route
-            path={ROUTES.MANAGE_EVENT_RESPONSES}
-            element={<ManageEventFormResponsesWithCurrentUser />}
-          />
+            <Route
+              path={ROUTES.MANAGE_EVENT_RESPONSES}
+              element={<ManageEventFormResponsesWithCurrentUser />}
+            />
 
-          <Route path={ROUTES.SEND_OUT} element={<SendOut />} />
-          <Route
-            path={ROUTES.EVENT_ECONOMY}
-            element={<EventEconomyPageWithCurrentUser />}
-          />
-          <Route
-            path={ROUTES.TICKET_SCANNER}
-            element={<TicketScannerPageWithCurrentUser />}
-          />
-        </Route>
-        <Route path={ROUTES.PRIVACY_POLICY} element={<PrivacyPolicy />} />
-        <Route path="*" element={<FourOFour404 />} />
-        <Route path="/404" element={<FourOFour404 />} />
-      </Routes>
+            <Route path={ROUTES.SEND_OUT} element={<SendOut />} />
+            <Route
+              path={ROUTES.EVENT_ECONOMY}
+              element={<EventEconomyPageWithCurrentUser />}
+            />
+            <Route
+              path={ROUTES.TICKET_SCANNER}
+              element={<TicketScannerPageWithCurrentUser />}
+            />
+          </Route>
+          <Route path={ROUTES.PRIVACY_POLICY} element={<PrivacyPolicy />} />
+          <Route path="*" element={<FourOFour404 />} />
+          <Route path="/404" element={<FourOFour404 />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ src/routes/page_import.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+export const MainPage = React.lazy(() => import("../pages/main"));
+export const ProfilePage = React.lazy(() => import("../pages/profile"));
+export const EditEventAddTicketReleasePage = React.lazy(
+  () => import("../pages/event/edit_ticket_release")
+);
+export const EditTicketTypes = React.lazy(
+  () => import("../pages/event/edit_ticket_types")
+);
+export const ProfileTicketRequestsPage = React.lazy(
+  () => import("../pages/profile/profile_ticket_requests")
+);
+export const ProfileTicketsPage = React.lazy(
+  () => import("../pages/profile/profile_tickets")
+);
+export const ProfileOrganizationsPage = React.lazy(
+  () => import("../pages/profile/profile_organizations")
+);
+export const CreateOrganizationPage = React.lazy(
+  () => import("../pages/organization/create")
+);
+export const ManageEventPage = React.lazy(
+  () => import("../pages/event/manage")
+);
+export const TicketScannerPage = React.lazy(
+  () => import("../pages/event/ticket_scanner")
+);
+export const EventEconomyPage = React.lazy(
+  () => import("../pages/event/economy")
+);
+export const EditTicketReleaseAddonsPage = React.lazy(
+  () => import("../pages/event/edit_addons")
+);
+export const ManageEventTicketReleasesPage = React.lazy(
+  () => import("../pages/event/manage/ticket_releases")
+);
+export const ManageEventTicketsPage = React.lazy(
+  () => import("../pages/event/manage/tickets")
+);
+export const ManageEventFormResponsesPage = React.lazy(
+  () => import("../pages/event/manage/ticket_form_reponse_list_page")
+);
+export const FourOFour404 = React.lazy(() => import("../pages/errors/404"));
+
+export const EventDetail = React.lazy(() => import("../pages/event/detail"));
+
+export const EventsPage = React.lazy(() => import("../pages/event"));
+
+export const CreateEventPage = React.lazy(
+  () => import("../pages/event/create")
+);
+
+export const EditEventPage = React.lazy(() => import("../pages/event/edit"));
+
+export const External = React.lazy(() => import("../pages/external"));
+export const WelcomePage = React.lazy(() => import("../pages/welcome"));
+export const ProtectedRoute = React.lazy(
+  () => import("../components/login/PrivateRoute")
+);
+export const Logout = React.lazy(() => import("../components/user/logout"));
+
+export const HandleLoginCallback = React.lazy(
+  () => import("../pages/login/callback")
+);
+export const ExternalVerifyEmail = React.lazy(
+  () => import("../pages/external/verify-email")
+);
+export const ContactPage = React.lazy(() => import("../pages/contact"));
+
+export const ForgotPassword = React.lazy(
+  () => import("../pages/external/forgot_password")
+);
+export const PasswordReset = React.lazy(
+  () => import("../pages/external/password_reset")
+);
+export const SendOut = React.lazy(
+  () => import("../components/events/send_out")
+);
+export const VerifyPreferredEmail = React.lazy(
+  () => import("../pages/user/preferred_email/verify")
+);
+
+export const PrivacyPolicy = React.lazy(
+  () => import("../components/legal/privacy")
+);
+
