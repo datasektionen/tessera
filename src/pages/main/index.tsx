a20b11cbc1161f86e8ac31a4b9bdba66792ff31f
Lucas Dow <lucdow7@gmail.com>
2024-07-11 17:20:16 +0900

    mörkläggning

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -237,16 +237,6 @@ const MainPage: React.FC = () => {
           position: "relative",
         }}
       >
-        <Box
-          ref={GetInTouchRef}
-          style={{
-            borderTop: "1px solid " + PALLETTE.cerise,
-            borderBottom: "1px solid " + PALLETTE.cerise,
-          }}
-          my={4}
-        >
-          <ContactDetails />
-        </Box>
         <Grid
           container
           justifyContent="center"
